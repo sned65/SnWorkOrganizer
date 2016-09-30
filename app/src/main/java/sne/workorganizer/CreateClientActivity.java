8d70fb44edabd410051c6090c4f999b3e882fd01
Nikolay Smirnov <nicksm65@gmail.com>
2016-09-30 18:17:00 +0400

    Work from 30.09.2016

--- app/src/main/java/sne/workorganizer/CreateClientActivity.java
+++ app/src/main/java/sne/workorganizer/CreateClientActivity.java
@@ -1,7 +1,11 @@
 package sne.workorganizer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class CreateClientActivity extends AppCompatActivity
 {
@@ -11,5 +15,58 @@ public class CreateClientActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_client);
+
+        startActionMode(new ActionMode.Callback()
+        {
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu)
+            {
+                getMenuInflater().inflate(R.menu.menu_create_client, menu);
+                mode.setTitle("New Client");
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu)
+            {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item)
+            {
+                switch (item.getItemId())
+                {
+                case R.id.btn_cancel:
+                    cancel();
+                    return true;
+                case R.id.btn_save:
+                    save();
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode)
+            {
+
+            }
+        });
+    }
+
+    private void cancel()
+    {
+        Intent result = new Intent();
+        setResult(RESULT_CANCELED, result);
+        finish();
+    }
+
+    private void save()
+    {
+        Intent result = new Intent();
+        //result.putExtra()
+        setResult(RESULT_OK, result);
+        finish();
     }
 }
